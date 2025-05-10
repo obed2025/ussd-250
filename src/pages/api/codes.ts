76ea5dcf450e3ddff34363a942f004d9ddca2b5a
NIYOMUGISHA Obed <obed6@duck.com>
2025-05-10 20:34:50 +0200

    Initial Commit

---
+++ src/pages/api/codes.ts
@@ -0,0 +1,60 @@
+import { Client, Databases, ID, type Models } from 'appwrite';
+import type { APIRoute } from 'astro';
+export const prerender = false;
+
+const {
+  APPWRITE_API_ENDPOINT,
+  APPWRITE_PROJECT_ID,
+  APPWRITE_DATABASE_ID,
+  APPWRITE_COLLECTION_ID,
+} = import.meta.env;
+
+const client = new Client();
+client.setEndpoint(APPWRITE_API_ENDPOINT).setProject(APPWRITE_PROJECT_ID);
+
+const databases = new Databases(client);
+
+export const GET: APIRoute = async ({ request }) => {
+  try {
+    const data = await databases.listDocuments(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_COLLECTION_ID
+    );
+
+    return new Response(
+      JSON.stringify({
+        total: data.total,
+        documents: data.documents.map(mapDocuments),
+      })
+    );
+  } catch (error) {
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
+};
+
+export const POST: APIRoute = async ({ request }) => {
+  if (request.headers.get('Content-Type') === 'application/json') {
+    try {
+      const body = await request.json();
+
+      const result = await databases.createDocument(
+        APPWRITE_DATABASE_ID,
+        APPWRITE_COLLECTION_ID,
+        ID.unique(),
+        body
+      );
+      return new Response(JSON.stringify(mapDocuments(result)));
+    } catch (error) {
+      return new Response(JSON.stringify(error), { status: 422 });
+    }
+  }
+
+  return new Response(null, { status: 400 });
+};
+
+function mapDocuments(document: Models.Document) {
+  const { $id, $createdAt, $updatedAt, code, title, description, MTN, Airtel } =
+    document;
+
+  return { $id, $createdAt, $updatedAt, code, title, description, MTN, Airtel };
+}
